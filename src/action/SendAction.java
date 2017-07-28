791522f3c53f5f061f36aaa237138e40f37b9bf4
yourname <2072414837@qq.com>
2017-07-28 11:27:49 +0800

    Removed servlet at /sendaction

--- src/action/SendAction.java
+++ src/action/SendAction.java
@@ -1,53 +1,16 @@
 package action;
-
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Date;
-
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import dbo.DBAccess;
-import log.Log;
-import logincheck.LoginCheck;
-import model.MessageInfo;
-import model.UserInfo;
-
-/**
- * Servlet implementation class SendAction
- */
-@WebServlet({ "/SendAction", "/sendaction", "/sendaction.html" })
+import javax.servlet.http.*;
+@Deprecated //操作已经移至send.jsp
 public class SendAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SendAction() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    public SendAction() {super();}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=utf-8;pageencoding=utf-8");
 		response.getWriter().append("请输入正确内容访问！");
 		response.addHeader("refresh", "2;url=index.html");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		//response.sendRedirect("send.jsp");
-	}
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {}
 }
